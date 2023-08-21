a7e39d3365932ed0689b4121ad8458492809f4ab
Larisa <laserova@yandex.ru>
2023-08-22 02:46:25 +0500

    IntegerList with selectionSort and binarySearch

--- src/main/java/org/laserova/IntegerListImpl.java
+++ src/main/java/org/laserova/IntegerListImpl.java
@@ -68,12 +68,12 @@ public class IntegerListImpl implements IntegerList {
 //        }
 //        size++;
 //        return storage[index] = item;
-        if (index==size){
-            storage[size++]=item;
+        if (index == size) {
+            storage[size++] = item;
             return item;
         }
-        System.arraycopy(storage,index,storage,index+1,size-index);
-        storage[index]=item;
+        System.arraycopy(storage, index, storage, index + 1, size - index);
+        storage[index] = item;
         size++;
         return item;
     }
@@ -82,7 +82,7 @@ public class IntegerListImpl implements IntegerList {
     public Integer set(int index, Integer item) {
         validateIndex(index);
         validateItem(item);
-        return storage[index]=item;
+        return storage[index] = item;
     }
 
     @Override
@@ -95,9 +95,9 @@ public class IntegerListImpl implements IntegerList {
     @Override
     public Integer remove(int index) {
         validateIndex(index);
-        Integer item =storage[index];
-        if (index!=size){
-            System.arraycopy(storage,index+1,storage,index,size-index);
+        Integer item = storage[index];
+        if (index != size) {
+            System.arraycopy(storage, index + 1, storage, index, size - index);
         }
         size--;
         return item;
@@ -105,14 +105,16 @@ public class IntegerListImpl implements IntegerList {
 
     @Override
     public boolean contains(Integer item) {
-        return indexOf(item)!=-1;
+        this.selectionSort();
+        return binarySearch(item)==-1;
+//        return indexOf(item) != -1;
     }
 
     @Override
     public int indexOf(Integer item) {
         validateItem(item);
         for (int i = 0; i < size; i++) {
-            if (storage[i].equals(item)){
+            if (storage[i].equals(item)) {
                 return i;
             }
         }
@@ -122,8 +124,8 @@ public class IntegerListImpl implements IntegerList {
     @Override
     public int lastIndexOf(Integer item) {
         validateItem(item);
-        for (int i = size; i >=0 ; i--) {
-            if (storage[i].equals(item)){
+        for (int i = size; i >= 0; i--) {
+            if (storage[i].equals(item)) {
                 return i;
             }
         }
@@ -176,6 +178,7 @@ public class IntegerListImpl implements IntegerList {
         return Arrays.copyOf(storage, size);
     }
 
+
     private void validateItem(Integer item) {
         if (item == null) {
             throw new NullItemException();
@@ -193,5 +196,35 @@ public class IntegerListImpl implements IntegerList {
         Integer[] newStorage = new Integer[length];
         if (size >= 0) System.arraycopy(storage, 0, newStorage, 0, size);
         storage = newStorage;
+    }
+
+    private void selectionSort() {
+        for (int i = 0; i < IntegerListImpl.this.storage.length - 1; i++) {
+            int minItemIndex = i;
+            for (int j = i + 1; j < IntegerListImpl.this.storage.length; j++) {
+                if (IntegerListImpl.this.storage[j] < IntegerListImpl.this.storage[minItemIndex]) {
+                    minItemIndex = j;
+                }
+            }
+            int transit = IntegerListImpl.this.storage[i];
+            IntegerListImpl.this.storage[i] = IntegerListImpl.this.storage[minItemIndex];
+            IntegerListImpl.this.storage[minItemIndex] = transit;
+        }
+    }
+
+    private int binarySearch (Integer item) {
+        int firstIndex = 0;
+        int lastIndex = this.storage.length - 1;
+        while(firstIndex <= lastIndex) {
+            int middleIndex = (firstIndex + lastIndex) / 2;
+            if (this.storage[middleIndex].equals(item)) {
+                return middleIndex;
+            }
+            else if (this.storage[middleIndex] < item)
+                firstIndex = middleIndex + 1;
+            else if (this.storage[middleIndex] > item)
+                lastIndex = middleIndex - 1;
+        }
+        return -1;
     }
 }
